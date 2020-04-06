f0e3af6efd0963b42411768169aec3413ad80805
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-06 10:51:17 +0530

    Implemented backend functionalities

---
+++ backend/db/migration.js
@@ -0,0 +1,24 @@
+const migartion = [`CREATE TABLE IF NOT EXISTS ride_info (
+    id INT PRIMARY KEY,
+    user_id INT,
+    vehicle_model_id INT,
+    package_id INT,
+    travel_type_id INT,
+    form_area_id INT,
+    to_area_id INT,
+    from_city_id INT,
+    to_city_id INT,
+    from_date TIMESTAMP,
+    to_date TIMESTAMP,
+    online_booking INT,
+    mobile_site_booking INT,
+    booking_created TIMESTAMP,
+    from_lat FLOAT(30),
+    from_long FLOAT(30),
+    to_lat FLOAT(30),
+    to_long FLOAT(30),
+    car_cancellation INT
+  )`
+]
+
+module.exports = migartion;
---
+++ backend/test.js
@@ -0,0 +1,29 @@
+// query insert into table_info (id, name, age) values ($1, $2, $3), ($4, $5, $6)
+// values : [1, 'asd', 21, 2, 'dfsd', 32]
+
+const arr = [
+    {
+        id: 10,
+        name: 'asd',
+        age: 21
+    },
+    {
+        id: 92,
+        name: 'dfsd',
+        age: 32
+    }
+]
+
+
+
+function test() {
+    for (let i = 0; i < arr.length; i++) {
+        let query = {
+            text: `INSERT INTO ride_info (id, name, age) VALUES)`,
+            values: [`${(arr[i].id)}, ${(arr[i].name)}, ${(arr[i].age)}`]
+        };
+        console.log(query);
+    }
+}
+
+test();
